cde3f9592ef410cbe1594e90c7d8249524185a84
Greg Johnston <greg.a.johnston@gmail.com>
2016-04-28 23:55:11 -0400

    Read files from url

--- prog_class.py
+++ prog_class.py
@@ -1,2 +1,25 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+
 def import_url():
-    return True
+    req = Request('http://rosettacode.org/wiki/Factorial',
+                  headers={'User-Agent': 'Mozilla/5.0'})
+    rosettaFile = urlopen(req)
+    rosettaHtml = rosettaFile.read()
+    rosettaFile.close()
+    soup = BeautifulSoup(rosettaHtml, 'html.parser')
+    return soup.get_text()
+
+
+def print_text():
+    req = Request('http://rosettacode.org/wiki/Factorial',
+                  headers={'User-Agent': 'Mozilla/5.0'})
+    rosettaFile = urlopen(req)
+    for line in rosettaFile:
+        print(line)
+
+    # rosettaHtml = rosettaFile.read()
+    # rosettaFile.close()
+    # soup = BeautifulSoup(rosettaHtml, 'html.parser')
+    # return soup.get_text()
--- prog_class_test.py
+++ prog_class_test.py
@@ -1,5 +1,26 @@
 from prog_class import import_url
+import unittest
 
 
-def test_import_url():
-    assert import_url()
+class TestStringMethods(unittest.TestCase):
+
+    def test_import_url(self):
+        temp = import_url()
+        self.assertEqual(type(temp), str)
+
+    # def test_upper(self):
+    #     self.assertEqual('foo'.upper(), 'FOO')
+
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
